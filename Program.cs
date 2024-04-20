e2b2109e0df9e5655829f367428316e986a86fd1
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-04-20 12:08:13 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lec_4_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Carla Baysinger
+            // Lecture 4 notes
+            // 04/20/2024
+
+            // April 12th Topics
+
+            // Quick Followup 
+            // Strings are weird
+            // Pedmas
+
+            // ---------------------------
+            // Floating Point Types ( float and double )
+            // Precision and Memory
+            // Decimal Type
+            // ---------------------------
+
+            // ---------------------------
+            // Revisit Parsing
+            // Casting
+            // Math with Doubles
+            // Rounding
+            // ---------------------------
+
+            // ---------------------------
+            // Question Examples
+            // Homework
+            // ----------------------------
+
+
+        }
+    }
+}
